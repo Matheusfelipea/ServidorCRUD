505e7aad879c29ae839ad7c4e35017975b9e86f2
Matheusfelipea <matheusfelipe_801@hotmail.com>
2022-04-03 15:29:20 -0300

    arquivos atividade

---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var aplicacao = express();
+let clientes = require("./clientes");
+aplicacao.use(express.json());
+aplicacao.use(express.urlencoded({extended:false}));
+
+
+aplicacao.get('/clientes', function (req, res){
+    
+    return res.status(200).send ('');
+});
+
+aplicacao.get('/clientes/:id', function (req, res){
+    var userId = req.params['id'];
+
+    return res.status(200).send ('');
+});
+
+aplicacao.post('/clientes/:id/:nome/:endereco/:email', function( req, res){
+
+});
+
+aplicacao.patch('/clientes/:id/:nome/:endereco/:email', function( req, res){
+
+});
+aplicacao.delete('/clientes/:id/', function( req, res){
+
+});
+
+aplicacao.listen(3000, function() {
+    console.log("Estou funcionando");
+});
